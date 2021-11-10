ff03c72a4789b0fcfafd4ab281b334417b937965
Evgeniy Nechaev <kit8@bk.ru>
2021-11-10 20:34:10 +0300

    V 1.1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,26 @@
 import kotlin.math.roundToInt
 
 var commissionVkPay = 0
-fun transfer() {
+var commission = 0.075
+fun transfer(amountMonth: Int, whatPaySystem: Int, amount: Double) {
+
+    val commissionMasterAndMaestro = if ((amountMonth in 301..74999)) 0.0 else (amount * 0.006 + 20)
+
+    val commissionVisaAndMir =if ((commission*amount) < 35) 35.0 else (commission*amount)
+
+    val price = when (whatPaySystem) {
+        1 -> amount - commissionMasterAndMaestro
+        2 -> amount - commissionMasterAndMaestro
+        3 -> amount - commissionVisaAndMir
+        4 -> amount - commissionVisaAndMir
+        5 -> amount - amount * commissionVkPay
+        else -> println("Ошибка")
+    }
+val finalPrice = price as Double
+    val amountTransaction = (finalPrice * 100.0).roundToInt() / 100.0
+    println("Вы перевели: $amountTransaction рублей")
+}
+fun main() {
     println("К какой платежной системы относится Ваша карта? ")
     val string = """1. Mastercard
             |2. Maestro
@@ -11,42 +30,18 @@ fun transfer() {
              """.trimMargin()
     println (string)
     val whatPaySystem = readLine()!!.toInt()
-
-    println("Какую сумму будете переводить?")
-    val amount = readLine()!!.toDouble()
-
     println("Укажите обущю сумму переводов за месяц:")
     val amountMonth = readLine()!!.toInt()
 
     println("Укажите обущю сумму переводов за день:")
     val amountDay = readLine()!!.toDouble()
 
-    val commission1 = if ((amountMonth in 301..74999)) 0 else (amount * 0.006 + 20)
-
-    val commission2 =0.005 //if ((commission*amount) < 35) 35 else (commission*amount)
-
-
-    val price = when (whatPaySystem) {
-        1 -> amount - (amount * commission1)
-        2 -> amount - (amount * commission1)
-        3 -> amount - (amount * commission2)
-        4 -> amount - (amount * commission2)
-        5 -> amount - amount * commissionVkPay
-        else -> println("Ошибка")
+    if (amountDay >= 150_000.00 || amountMonth >= 600_000.0) {
+        println("Лимиты переводов превышены, операция отменена")
+    } else {
+        println("Какую сумму будете переводить?")
+        val amount = readLine()!!.toDouble()
+        transfer(amountMonth, whatPaySystem, amount)
     }
-val finalPrice = price as Double
-    val amountTransaction = (finalPrice * 100.0).roundToInt() / 100.0
-    println("Вы перевели: $amountTransaction рублей")
 }
 
-private operator fun Double.minus(any: Any): Any {
-    TODO("Not yet implemented")
-}
-
-private operator fun Double.times(commission2: Number): Any {
-    TODO("Not yet implemented")
-}
-
-fun main() {
-    transfer()
-}
